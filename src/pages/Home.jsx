836fba0c68d0a6320f8f4f29e7b7fa7c59309fc5
Dibakar-Biswas <dibakarbiswas742@gmail.com>
2025-11-09 23:15:15 +0530

    added latest model

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,24 @@
-import React from 'react';
-import Slider from '../components/Slider';
+import React from "react";
+import Slider from "../components/Slider";
+import { useLoaderData } from "react-router";
+import ModelCard from "../components/ModelCard";
 
 const Home = () => {
-    return (
-        <div>
-            <Slider />
-        </div>
-    );
+  const data = useLoaderData();
+  console.log(data);
+  return (
+    <div>
+      <Slider />
+
+      <div className="text-2xl text-center font-bold mt-5">Featured AI Models</div>
+
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3">
+        {data.map((model) => (
+          <ModelCard key={model._id} model={model}></ModelCard>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Home;
--- src/routes/Route.jsx
+++ src/routes/Route.jsx
@@ -18,6 +18,7 @@ export const router = createBrowserRouter([
       {
         path: "/",
         element: <Home></Home>,
+        loader: () => fetch('http://localhost:4000/latest-models')
       },
       {
         path: "/add-model",
